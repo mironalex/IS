d135880355381ad26ebc7924b5972044f202a39f
Alexandru Miron <mironalex96@gmail.com>
2017-12-20 13:37:33 +0200

    finished key manager

--- NodeKM.py
+++ NodeKM.py
@@ -1,11 +1,28 @@
 import socket
+from Crypto.Cipher import AES
+from Crypto import Random
 
 
 PORT = 4343
+PADDING = '_'
+BLOCK_SIZE = 16
 
 key1 = "1"
 key2 = "2"
 key3 = "3"
+
+
+def pad(x):
+    return x + (BLOCK_SIZE - len(x) % BLOCK_SIZE) * PADDING
+
+
+def AESencrypt(plaintext, key):
+    key = pad(key)
+    plaintext = pad(plaintext)
+    cipher = AES.new(key)
+    encryptedtext = cipher.encrypt(plaintext)
+    return encryptedtext
+
 
 # start server
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -16,16 +33,19 @@ print("Server is running.")
 while True:
     client, address = server.accept()
     print(str(address) + "connected.")
-
     data = client.recv(10).decode()
+    encryptedkey = ""
 
     if data == "ECB":
-        pass
+        encryptedkey = AESencrypt(key1, key3)
+
 
     elif data == "CFB":
-        pass
+        encryptedkey = AESencrypt(key2, key3)
 
     else:
         client.close()
         continue
 
+    client.send(encryptedkey)
+    client.close()
