4de0e0fa03b77ac523ddfc6253f6214378224311
Alexandru Miron <mironalex96@gmail.com>
2017-12-21 18:44:51 +0200

    implemented ECB encryption/decryption

--- NodeA.py
+++ NodeA.py
@@ -9,12 +9,15 @@ KM_ADDRESS = '127.0.0.1'
 B_ADDRESS = '127.0.0.1'
 KM_PORT = 4343
 B_PORT = 4344
+FILE_PATH = "./testfile"
 
 KEY3 = '3'
 
 
 def pad(x):
-    return x + (BLOCK_SIZE - len(x) % BLOCK_SIZE) * PADDING
+    if len(x) % BLOCK_SIZE != 0:
+        return x + (BLOCK_SIZE - len(x) % BLOCK_SIZE) * PADDING
+    return x
 
 
 def AESencrypt(plaintext, key):
@@ -30,33 +33,39 @@ def AESdecrypt(encryptedtext, key):
     cipher = AES.new(key)
     return cipher.decrypt(encryptedtext)
 
+if __name__ == "__main__":
+    # tell B the operating mode
+    conn_to_b = socket.socket()
+    conn_to_b.connect((B_ADDRESS, B_PORT))
+    conn_to_b.send(b'ECB')
+    print('Conenction to B established')
 
-# tell B the operating mode
-conn_to_b = socket.socket()
-conn_to_b.connect((B_ADDRESS, B_PORT))
-conn_to_b.send(b'ECB')
-print('Conenction to B established')
 
+    # get corresponding key from KM
+    conn_to_km = socket.socket()
+    conn_to_km.connect((KM_ADDRESS, KM_PORT))
+    conn_to_km.send(bytes(MODE))
+    print('Connection to KM established')
+    encrypted_key = conn_to_km.recv(BLOCK_SIZE)
+    conn_to_km.close()
 
-# get corresponding key from KM
-conn_to_km = socket.socket()
-conn_to_km.connect((KM_ADDRESS, KM_PORT))
-conn_to_km.send(bytes(MODE))
-print('Connection to KM established')
-encrypted_key = conn_to_km.recv(BLOCK_SIZE)
-conn_to_km.close()
+    key = AESdecrypt(encrypted_key, KEY3)
+    print('decrypted key: ' + str(key))
 
-key = AESdecrypt(encrypted_key, KEY3)
-print('decrypted key: ' + str(key))
+    # wait for ready message
+    ready_message = conn_to_b.recv(5)
 
-# wait for ready message
-ready_message = conn_to_b.recv(5)
+    if ready_message == b'READY':
+        # start sending file
+        if MODE == 'ECB':
+            with open(FILE_PATH, "rb+") as f:
+                block = f.read(BLOCK_SIZE)
+                while len(block) != 0:
+                    encrypted_block = AESencrypt(block, key)
+                    conn_to_b.send(encrypted_block)
+                    block = f.read(BLOCK_SIZE)
 
-if ready_message == b'READY':
-    # start sending file
-    pass
+        if MODE == 'CFB':
+            pass
 
-else:
     conn_to_b.close()
-    exit()
-
--- NodeB.py
+++ NodeB.py
@@ -9,12 +9,15 @@ KM_ADDRESS = '127.0.0.1'
 B_ADDRESS = '127.0.0.1'
 KM_PORT = 4343
 B_PORT = 4344
+FILE_PATH = './outputfile'
 
 KEY3 = '3'
 
 
 def pad(x):
-    return x + (BLOCK_SIZE - len(x) % BLOCK_SIZE) * PADDING
+    if len(x) % BLOCK_SIZE != 0:
+        return x + (BLOCK_SIZE - len(x) % BLOCK_SIZE) * PADDING
+    return x
 
 
 def AESencrypt(plaintext, key):
@@ -40,5 +43,28 @@ server.listen(1)
 # wait for mode message from node A
 client, address = server.accept()
 MODE = bytes.decode(client.recv(3))
+
+conn_to_km = socket.socket()
+conn_to_km.connect((KM_ADDRESS, KM_PORT))
+conn_to_km.send(bytes(MODE))
+print('Key reqeuested from KM')
+encrypted_key = conn_to_km.recv(BLOCK_SIZE)
+conn_to_km.close()
+
+key = AESdecrypt(encrypted_key, KEY3)
+
+with open(FILE_PATH, "wb+") as f:
+    client.send(b'READY')
+    if MODE == 'ECB':
+        encrypted_block = client.recv(BLOCK_SIZE)
+        while len(encrypted_block) != 0:
+            decrypted_block = AESdecrypt(encrypted_block, key)
+            f.write(decrypted_block)
+            encrypted_block = client.recv(BLOCK_SIZE)
+
+    if MODE == 'CFB':
+        pass
+
+server.close()
 
 
